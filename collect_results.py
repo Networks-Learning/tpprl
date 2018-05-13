1e21398936419b14bf967887dac187cf42f689a1
Utkarsh Upadhyay <musically.ut@gmail.com>
2018-05-13 12:07:49 +0200

    Make data collection script more robust.

--- collect_results.py
+++ collect_results.py
@@ -12,6 +12,7 @@ import dill
 import multiprocessing as MP
 import redqueen.utils as RU
 import redqueen.opt_runs as OR
+import redqueen.opt_model as OM
 # import tpprl.read_data_utils as RDU
 import tpprl.exp_broadcaster as EB
 import natsort
@@ -22,6 +23,8 @@ save_dir_tmpl = 'train-save-user_idx-{}'
 save_dir_regex = re.compile(r'train-save-user_idx-(\d*)')
 save_dir_glob = r'train-save-user_idx-*'
 user_data = None
+MAX_EVENTS = 20000
+MAX_ITERS = 50
 
 
 def read_user_data(user_data_file):
@@ -31,9 +34,12 @@ def read_user_data(user_data_file):
 
 
 def worker_user(params):
-    (user_idx, output_dir, test_batches, RQ_cap_adjust) = params
+    (user_idx, output_dir, test_batches, RQ_cap_adjust, for_epoch, verbose) = params
 
     save_dir = os.path.join(output_dir, save_dir_tmpl.format(user_idx))
+
+    if verbose:
+        print('Working on user_idx: {}'.format(user_idx))
 
     with open(os.path.join(save_dir, 'user_opt_dict.dill'), 'rb') as f:
         user_opt_dict = dill.load(f)
@@ -65,14 +71,21 @@ def worker_user(params):
     # So to extract the last checkpoint, we do a sort by real-values and
     # pick the most negative value.
     # Also, we drop the `.meta` suffix.
-    all_chpt_file = glob.glob(os.path.join(save_dir, '*.meta'))
-    last_chpt_file = natsort.realsorted(all_chpt_file)[0][:-5]
 
-    ret['chpt_file'] = last_chpt_file
+    if for_epoch < 0:
+        all_chpt_file = glob.glob(os.path.join(save_dir, '*.meta'))
+        chosen_chpt_file = natsort.realsorted(all_chpt_file)[0][:-5]
+    else:
+        chosen_chpt_file = os.path.join(save_dir, 'tpprl.ckpt-{}'.format(for_epoch))
+
+    ret['chpt_file'] = chosen_chpt_file
+    if not os.path.exists(chosen_chpt_file + '.meta'):
+        ret['error'] = 'File Not Found: {}.'.format(chosen_chpt_file)
+        return ret
 
     rl_b_dict = EB.rl_b_dict_from_chpt(
         # '/NL/crowdjudged/work/rl-broadcast/r_2-sim-opt-fix/train-save-user_idx-218/tpprl.ckpt-898',
-        last_chpt_file,
+        chosen_chpt_file,
         one_user_data=one_user_data,
         window_start=window_start,
         user_opt_dict=user_opt_dict
@@ -97,7 +110,7 @@ def worker_user(params):
             batch_sim_opt=eval_sim_opts,
             seed=init_seed + idx
         )
-        mgr.run_dynamic()
+        mgr.run_dynamic(max_events=MAX_EVENTS)
         rl_dfs.append(mgr.get_state().get_dataframe())
 
     num_tweets = [RU.num_tweets_of(df, broadcaster_id=eval_sim_opts.src_id)
@@ -110,28 +123,33 @@ def worker_user(params):
     # Figure out what 'q' to use for RQ to get the same number of tweets.
     # Removing '1' because RQ systematically tweets more.
     q_RQ = RU.sweep_q(eval_sim_opts, capacity_cap=capacity_cap - RQ_cap_adjust,
-                      verbose=False, q_init=q, parallel=False)
+                      verbose=verbose, q_init=q, parallel=False, max_events=MAX_EVENTS,
+                      max_iters=MAX_ITERS)
     ret['q_RQ'] = q_RQ
 
     # Run RedQueen.
     RQ_dfs = []
+    RQ_events = []
     for idx in range(test_batches):
-        mgr = eval_sim_opts.update({'q': q_RQ}).create_manager_with_opt(seed=init_seed + idx)
+        opt = OM.Opt(src_id=eval_sim_opts.src_id, seed=init_seed + idx, s=eval_sim_opts.s, q=q_RQ)
+        mgr = eval_sim_opts.update({'q': q_RQ}).create_manager_with_broadcaster(opt)
         # mgr = eval_sim_opts.update({}).create_manager_with_opt(seed=init_seed + idx)
         mgr.state.time = window_start
-        mgr.run_dynamic()
+        mgr.run_dynamic(max_events=MAX_EVENTS)
         RQ_dfs.append(mgr.get_state().get_dataframe())
+        RQ_events.append(opt.state.events)
 
     # Run Poisson.
     poisson_dfs = []
+    poisson_events = []
+    rate = capacity_cap / (eval_sim_opts.end_time - window_start)
     for idx in range(test_batches):
-        mgr = eval_sim_opts.create_manager_with_poisson(
-            rate=capacity_cap / (eval_sim_opts.end_time - window_start),
-            seed=init_seed + idx
-        )
+        poisson = OM.Poisson2(src_id=eval_sim_opts.src_id, seed=init_seed + idx, rate=rate)
+        mgr = eval_sim_opts.create_manager_with_broadcaster(poisson)
         mgr.state.time = window_start
-        mgr.run_dynamic()
+        mgr.run_dynamic(max_events=MAX_EVENTS)
         poisson_dfs.append(mgr.get_state().get_dataframe())
+        poisson_events.append(poisson.state.events)
 
     # Running Karimi
     K = 1
@@ -140,7 +158,7 @@ def worker_user(params):
     num_segments = 10
     seg_len = T / num_segments
     wall_mgr = eval_sim_opts.create_manager_for_wall()
-    wall_mgr.run_dynamic()
+    wall_mgr.run_dynamic(max_events=MAX_EVENTS)
     wall_df = wall_mgr.state.get_dataframe()
 
     ret['num_segments'] = num_segments
@@ -155,20 +173,24 @@ def worker_user(params):
     wall_intensities = wall_intensities_df[list(range(num_segments))].values
 
     # This is the single-threaded version
-    karimi_dfs = []
     params = (init_seed, capacity_cap, num_segments, eval_sim_opts, wall_intensities, None)
-    op = OR.worker_kdd(params, verbose=False, Ks=[K], window_start=window_start)
+    op = OR.worker_kdd(params, verbose=verbose, Ks=[K], window_start=window_start)
 
+    karimi_dfs = []
+    karimi_events = []
     for idx in range(test_batches):
-        piecewise_const_mgr = eval_sim_opts.create_manager_with_piecewise_const(
-            seed=init_seed + idx,
+        piecewise = OM.PiecewiseConst(
+            src_id=eval_sim_opts.src_id,
+            seed=init_seed * 2 + idx,
             change_times=window_start + np.arange(num_segments) * seg_len,
             rates=op['kdd_opt_{}'.format(K)] / seg_len
         )
+        piecewise_const_mgr = eval_sim_opts.create_manager_with_broadcaster(piecewise)
         piecewise_const_mgr.state.time = window_start
-        piecewise_const_mgr.run_dynamic()
+        piecewise_const_mgr.run_dynamic(max_events=MAX_EVENTS)
         df = piecewise_const_mgr.state.get_dataframe()
         karimi_dfs.append(df)
+        karimi_events.append(piecewise.state.events)
 
     # Calculating metrics
     metric_name = 'num_tweets'
@@ -201,11 +223,17 @@ def worker_user(params):
 @click.option('--batches', 'batches', help='How large should the test batches be.', default=64)
 @click.option('--force/--no-force', 'force', help='Whether to overwrite the output-csv file.', default=False)
 @click.option('--RQ-cap-adjust', 'RQ_cap_adjust', help='How much to compensate the tweets of RedQueen by.', default=1.0)
-def run(output_dir, save_csv, tweeters_data_file, batches, force, RQ_cap_adjust):
+@click.option('--for-epoch', 'for_epoch', help='Whether to produce the CSV for a given epoch. Negative numbers of running it on the latest epoch.', default=-1)
+@click.option('--parallel/--no-parallel', 'parallel', help='Whether to run on Multi-processing.', default=True)
+@click.option('--verbose/--no-verbose', 'verbose', help='Verbose mode.', default=False)
+def run(output_dir, save_csv, tweeters_data_file, batches, force, RQ_cap_adjust, for_epoch, parallel, verbose):
     """Read all OUTPUT_DIR and compile the results for all users and save them in SAVE_CSV.
     The user data is read from IN_DATA_FILE and `batches` number of batches are executed.
     """
+    cmd(output_dir, save_csv, tweeters_data_file, batches, force, RQ_cap_adjust, for_epoch, parallel, verbose)
 
+
+def cmd(output_dir, save_csv, tweeters_data_file, batches, force, RQ_cap_adjust, for_epoch, parallel, verbose):
     if os.path.exists(save_csv) and not force:
         print('File {} exists and --force was not supplied.'.format(save_csv))
         sys.exit(-1)
@@ -215,9 +243,12 @@ def run(output_dir, save_csv, tweeters_data_file, batches, force, RQ_cap_adjust)
     save_dirs = glob.glob(os.path.join(output_dir, save_dir_glob))
     user_idxes = [int(save_dir_regex.search(x)[1]) for x in save_dirs]
 
-    all_params = [(x, output_dir, batches, RQ_cap_adjust) for x in user_idxes]
-    with MP.Pool() as pool:
-        save_dict = pool.map(worker_user, all_params)
+    all_params = [(x, output_dir, batches, RQ_cap_adjust, for_epoch, verbose) for x in user_idxes]
+    if parallel:
+        with MP.Pool() as pool:
+            save_dict = pool.map(worker_user, all_params)
+    else:
+        save_dict = [worker_user(param) for param in all_params]
 
     save_df = pd.DataFrame(save_dict)
     save_df.to_csv(save_csv, index=False)
