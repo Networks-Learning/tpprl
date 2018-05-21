a950c363c4947238c04f1732a27be88ae4fa3dd7
Utkarsh Upadhyay <musically.ut@gmail.com>
2018-05-21 22:05:22 +0200

    Remove hardcoded folder paths.

--- sbatch/r_2_job.sh
+++ sbatch/r_2_job.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 source conda.sh
 source activate tf-cpu
 
-cd /home/utkarshu/prog/work/broadcast-rl/;
+cd ${HOME}/prog/work/broadcast-rl/;
 
 in_file=$1
 user_idx=$2
@@ -19,5 +19,3 @@ algo_feed=$9
 algo_approx=${10}
 
 python rq-compare-algo.py "${in_file}" "${user_idx}" "${out_dir}" --N $N --q $q --reward r_2_reward --until ${until} --epochs ${epochs} --restore --only-cpu --save-every $save_every $algo_feed $algo_approx
-
-# time ./rq-compare.py "/NL/redqueen/work/rl-broadcast/users-1k-HR-5-followers-pruned-200-own-posts-trimmed-2.dill" 904 "/NL/crowdjudged/work/rl-broadcast/r_2/" --N 300 --q 100.0  --reward r_2_reward --reward-top-k 1 --until 1000  --epochs 10 --allow-growth
--- sbatch/top_k_job.sh
+++ sbatch/top_k_job.sh
@@ -6,7 +6,7 @@ set -eo pipefail
 source conda.sh
 source activate tf-cpu
 
-cd /home/utkarshu/prog/work/broadcast-rl/;
+cd ${HOME}/prog/work/broadcast-rl/;
 
 in_file=$1
 user_idx=$2
@@ -21,5 +21,3 @@ algo_feed=${10}
 algo_approx=${11}
 
 python rq-compare-algo.py "${in_file}" "${user_idx}" "${out_dir}" --N $N --q $q --reward top_k_reward --until ${until} --epochs ${epochs} --restore --only-cpu --reward-top-k $K --save-every $save_every $algo_feed $algo_approx
-
-# time ./rq-compare.py "/NL/redqueen/work/rl-broadcast/users-1k-HR-5-followers-pruned-200-own-posts-trimmed-2.dill" 904 "/NL/crowdjudged/work/rl-broadcast/r_2/" --N 300 --q 100.0  --reward r_2_reward --reward-top-k 1 --until 1000  --epochs 10 --allow-growth
